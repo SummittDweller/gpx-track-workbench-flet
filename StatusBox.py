239a7e1e5e907e888a2ca7aedba4207f8364271e
SummittDweller <mark.mcfate@icloud.com>
2025-01-16 12:26:01 -0600

    Ok, reset is working along with our two status boxes

--- StatusBox.py
+++ StatusBox.py
@@ -1,16 +1,45 @@
-# Create the StatusBox class
+# StatusBox Class
+#---------------------------------------------------------------------------
+# This class, used in a Streamlit app, can place an st.empty( ) container in 
+# the app's sidebar interface.  
+# 
+# StatusBox.StatusBox(box_name) class constructor
+# ----
+# Creates an st.empty( ) container in the sidebar.  StatusBox attributes .name, 
+# .text. and .container are popluated but the named box is not visible until 
+# .update( ) is called.
+# 
+# update(msg, type='info')
+# ----
+# Use the 'update' method to change the st.empty( ) content.  The message can 
+# be formatted as 'info' (the default), 'success', 'warning' or 'error' as desired.
+#
+# Example use: 
+#
+# with st.sidebar:
+#     selected = option_menu("GPX Track Workbench", [ c.HOME, c.UPLOAD, c.EDIT, c.MAP, c.SPEED, c.POST ], 
+#     icons=['house-fill', 'cloud-upload-fill', 'pencil', 'map', 'speedometer', 'signpost-split'], 
+#     menu_icon="cast", default_index=0, on_change=on_change, key='main_menu')  
+#
+#     # Add uploader status text below the menu
+#     us = SB.StatusBox('uploader_status')
+#     us.update('Is this working?')
+#     time.sleep(5)
+#     f.state('uploader_status').update('Yes it is!')
+#
 
 import streamlit as st
 
 class StatusBox(object):
     
     # Constructor
-    def __init__(self, box='status_box', initial_message="Undefined"):
+    def __init__(self, box='status_box'):
         self.name = box
-        self.text = initial_message
+        self.text = f"{box}: Initialized"
         with st.sidebar:
-            self.container = st.empty( )        # create a Streamlit st.empty( ) container in the sidebar
+            self.container = st.empty( )   # create a Streamlit st.empty( ) container in the sidebar                
             self.container.warning(self.text)        # write our text in the container as a warning
+        st.session_state[box] = self
 
     # update the container message
     def update(self, msg, type='info'):
--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 # GPX-Track-Workbench/app.py
 
+# Key state( ) elements -----------------------------------------------
+#
+# uploader_status - A StatusBox object in the sidebar just below the menu
+# working_status - A StatusBox object in the sidebar below the uploader_status container
+
 import constants as c
 import functions as f
 import uploader as u
@@ -30,15 +35,16 @@ import time
 def on_change(key):
     selection = st.session_state[key]
     st.success(f"Selection changed to: {selection}")
+    st.session_state
 
     # Act on the selected key
     match selection:
-        case c.HOME:
-            st.warning("HOME not available")
+        case c.HOME:    # Delete all the items in Session state to reset EVERYTHING
+            for key in st.session_state.keys( ):
+                del st.session_state[key]
         case c.UPLOAD:
             st.warning("UPLOAD not available")
-            st.session_state.uploader_status_text = "You called for the Uploader!"
-            f.state('uploader_status').success(f.state('uploader_status_text'))
+            f.state('uploader_status').update(f"UPLOAD is not available!", 'error')
         case c.EDIT:
             st.warning("EDIT not availabe")
         case c.MAP:
@@ -62,7 +68,13 @@ def init_state( ):
     if not f.state('uploader_key'): 
         st.session_state.uploader_key = 0   # initialzie file_uploader key so it can be easily reset
 
+    # UI containers
+    if not f.state('uploader_status'):
+        SB.StatusBox('uploader_status')  # Call the class constructor 
+    if not f.state('working_status'):
+        SB.StatusBox('working_status')  # Call the class constructor 
 
+    # Unused for now
     if not f.state('prepared'):
         st.session_state.prepared = False 
     if not f.state('working_gpx'):
@@ -77,18 +89,6 @@ def init_state( ):
         st.session_state.process = None
     if not f.state('print_state_checkbox'):
         st.session_state.print_state_checkbox = None 
-
-    # UI containers
-    if not f.state('uploader_status'):
-        st.session_state.uploader_status = None 
-    if not f.state('uploader_status_text'):
-        st.session_state.uploader_status.text = "Uploader status is currently UNKNOWN" 
-    if not f.state('working_status'):
-        st.session_state.working_status = None 
-    if not f.state('working_status_text'):
-        st.session_state.working_status_text = "WorkingGPX status is currently UNKNOWN"
-
-    # Unused for now
     if not f.state('gpx_center'):
         st.session_state.gpx_center = c.MAP_CENTER
     if not f.state('posted_to_local'):
@@ -107,13 +107,13 @@ with st.sidebar:
     icons=['house-fill', 'cloud-upload-fill', 'pencil', 'map', 'speedometer', 'signpost-split'], 
     menu_icon="cast", default_index=0, on_change=on_change, key='main_menu')  
 
-    # Add uploader status text below the menu
-    us = st.session_state.uploader_status = SB.StatusBox('uploader_status')
-    us.update('Is this working?')
+    # Initialize our state variables!
+    init_state( )
 
-    # Add WorkingGPX status text below the uploader status
-    ws = st.session_state.working_status = SB.StatusBox('working_status')
-    ws.update('Yes it is!', 'success')
+# # Test the ability to change 'uploader_status' using the state variable.  It works!
+# f.state('uploader_status').update("This text uses the 'uploader_status' state( ) variable.")
+# # Now, set the working_status text using the stored uploader_status text as an error
+# f.state('working_status').update(f.state('uploader_status').text, 'error')
 
 # Do some things in the main area
 st.write(selected)
